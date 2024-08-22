b85135a7dac79d3e873868d142b4a65bafe42eca
amirrzw <amirrezaabotalebi81@gmail.com>
2024-08-22 14:19:45 +0330

    Switch database to PostgreSQL

--- BitpinTask/settings.py
+++ BitpinTask/settings.py
@@ -85,11 +85,16 @@ WSGI_APPLICATION = "BitpinTask.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bitpin',
+        'USER': 'postgres',
+        'PASSWORD': '1381amirreza',
+        'HOST': 'localhost',  # or the appropriate host if using a different one
+        'PORT': '5432',        # default PostgreSQL port
     }
 }
+
 
 
 # Password validation
